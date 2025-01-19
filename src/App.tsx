be987268d2edd96e1d089dc0fd145d2d203d8503
sami855-ux <samitale56@outlook.com>
2025-01-19 21:28:31 +0500

    Scafolding the floders

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
+  const startTrivial = async () => {}
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+
+  const nextQuestion = () => {}
   return <div>hello</div>
 }
